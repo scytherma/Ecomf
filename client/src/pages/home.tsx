1fea3edfe5356a7ab738710152a17ed065587060
calculadora-shopee <dantesparda906@gmail.com>
2025-12-06 04:43:02 -0300

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -757,7 +757,7 @@ export default function Home() {
                             <img 
                               src={module.image} 
                               alt={`Módulo ${module.title}`}
-                              className="absolute inset-0 object-cover group-hover:scale-105 transition-transform duration-500"
+                              className="absolute inset-0 w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
                             />
                           </div>
                         )}
